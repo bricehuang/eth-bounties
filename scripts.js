631b63261f371b427027f526c257e700f3c9f13e
Brian Gu <bgu@Tshepos-iMac.hsd1.ca.comcast.net>
2019-10-08 17:44:34 -0700

    more wip so brice can rebase

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
-$('div.bounty').hover(
-    function() {
+$(document).ready(function () {
+  $('div.bounty').hover(
+      function () {
         $('p', this).toggleClass('desc-active');
+        $('h3', this).toggleClass('desc-active');
         $(this).toggleClass('bounty-active');
-    });
+      });
+});
