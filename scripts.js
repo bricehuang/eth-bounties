55d3d2d7feb5ddc00b8396cd76d2eead92f0602c
Brian Gu <bgu@Tshepos-iMac.hsd1.ca.comcast.net>
2019-10-08 18:01:20 -0700

    minmal bounties page

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const BOUNTIES = [
   {
-    "title":"Legendre PRF Bounties",
-    "link":"https://legendreprf.org/bounties",
-    "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    "setter":"Ethereum Foundation",
-    "amount":"$14,000 USD",
+    "title": "Legendre PRF Bounties",
+    "link": "https://legendreprf.org/bounties",
+    "description": "",
+    "setter": "Ethereum Foundation",
+    "amount": "$14,000 USD",
   },
 ];
 
@@ -13,8 +13,8 @@ $.each(BOUNTIES, function(i, bounty){
     "<div class='bounty' onclick='location.href =" + bounty.link + " ;' >" +
       "<h3>" + bounty.title + "</h3>" +
       "<p>" + bounty.description + "</p>" +
-      "<p class='bounty-setter'>Posted By: " + bounty.setter + "</p>" +
-      "<p class='bounty-amount'>Total Bounty: " + bounty.amount + "</p>" +
+      "<div class='bounty-metadata'><p>Posted By: " + bounty.setter + "<br>" +
+      "Total Bounty: " + bounty.amount + "</p></div>" +
     "</div>"
   );
 });
