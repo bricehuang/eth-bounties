115d338db9e9955e3e7fa4c204ebd3d57d278b3b
Brice Huang <bmhuang@mit.edu>
2019-10-25 22:33:29 -0700

    title challenges.ethereum.org, remove subtitle, remove joke bounties except lorem ipsum

--- scripts.js
+++ scripts.js
@@ -14,27 +14,6 @@ const BOUNTIES = [
     "amount": "$30,000 USD",
   },
   {
-    "title": "A Conjecture of Bernhard Riemann",
-    "link": "https://www.claymath.org/millennium-problems/riemann-hypothesis",
-    "description": "It is widely believed that the zeroes of the Riemann zeta function lie on the line Re(z) = 1/2. Prove or disprove this conjecture.",
-    "setter": "Clay Mathematics Institute",
-    "amount": "$1,000,000 USD",
-  },
-  {
-    "title": "P ?= NP",
-    "link": "http://www.claymath.org/millennium-problems/p-vs-np-problem",
-    "description": "P is the set of languages that are decidable by a Turing Machine in polynomial time, and NP is the set of languages that are decidable by a non-deterministic Turing Machine in polynomial time. Do these complexity classes coincide?",
-    "setter": "Clay Mathematics Institute",
-    "amount": "$1,000,000 USD",
-  },
-  {
-    "title": "A Very Simple Function Applied to Positive Integers Sequentially",
-    "link": "https://en.wikipedia.org/wiki/Collatz_conjecture",
-    "description": "Take any positive integer n. If it is even, divide it by two. If it is odd, multiply it by three and add one. Repeat this procedure ad infinitum. Is it true for all n that you arrive at 1 in finitely many steps?",
-    "setter": "Paul Erdos",
-    "amount": "$500 USD",
-  },
-  {
     "title": "Lorem ipsum dolor...",
     "link": "https://loremipsum.io",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget arcu dictum varius duis at consectetur lorem donec. A iaculis at erat pellentesque. Et tortor at risus viverra adipiscing at in.",
